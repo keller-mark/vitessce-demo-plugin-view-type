db2853faef8464500805b906b6be3c3a6c8c054c
mkeller <7525285+keller-mark@users.noreply.github.com>
2022-04-19 14:34:04 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import {
+  Vitessce,
+  CoordinationType,
+  registerPluginViewType,
+  TitleInfo,
+  useCoordination,
+} from 'vitessce';
+import { myViewConfig } from './my-view-config';
+import 'vitessce/dist/es/production/static/css/index.css';
+import './index.css';
+
+function MyPluginView(props) {
+  const {
+    spatialZoom,
+    setSpatialZoom,
+  } = props;
+
+  function handleClick() {
+    setSpatialZoom(-10 + Math.random()*10);
+  }
+  return (
+    <div>
+      <p>Zoom level: <b>{spatialZoom}</b></p>
+      <p>
+        <button onClick={handleClick}>Try a random zoom level</button>
+      </p>
+    </div>
+  );
+}
+
+function MyPluginViewSubscriber(props) {
+  const {
+    coordinationScopes,
+    removeGridComponent,
+    theme,
+    title = 'My plugin view',
+  } = props;
+
+  // Get "props" from the coordination space.
+  const [{
+    spatialZoom
+  }, {
+    setSpatialZoom,
+  }] = useCoordination(
+    [
+      CoordinationType.DATASET,
+      CoordinationType.SPATIAL_ZOOM,
+    ],
+    coordinationScopes,
+  );
+
+  return (
+    <TitleInfo
+      title={title}
+      theme={theme}
+      removeGridComponent={removeGridComponent}
+      isReady={true}
+    >
+      <MyPluginView
+        spatialZoom={spatialZoom}
+        setSpatialZoom={setSpatialZoom}
+      />
+    </TitleInfo>
+  );
+}
+
+// Register the plugin view type.
+registerPluginViewType(
+  'myCustomZoomController',
+  MyPluginViewSubscriber,
+  [
+    CoordinationType.DATASET,
+    CoordinationType.SPATIAL_ZOOM,
+  ]
+);
+
+export default function App() {
+  return (
+    <Vitessce
+      config={myViewConfig}
+      height={800}
+      theme="light"
+    />
+  );
+}
